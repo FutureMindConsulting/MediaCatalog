de1a31e1a1c339b635fb79a1639d1125ab6ae6fc
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2020-11-30 12:01:13 +0100

    Some changes

--- MediaCatalog .cs
+++ MediaCatalog .cs
@@ -6,5 +6,21 @@ namespace MediaCatalog
 {
     class MediaCatalog
     {
+        int _nextMediaID;
+        Dictionary<int, Media> medias;
+
+        public int NextMediaID { get { return _nextMediaID++; } }
+        
+        public MediaCatalog()
+        {
+            _nextMediaID = 0;
+            medias = new Dictionary<int, Media>();
+        }
+
+        public void AddMedia(Media aMedia)
+        {
+            medias.Add(aMedia.MediaID, aMedia);
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,25 +7,35 @@ namespace MediaCatalog
         static void Main(string[] args)
         {
             Console.WriteLine("Media catalog starting");
-            Media m1 = new Media(1) { Author = "H.C. Andersen", 
-                                      Publisher = "Gyldendal", 
-                                      Title = "Ugly Duckling", 
-                                      Year = 2018 };
-            Media m2 = new Media(2)
+
+            MediaCatalog mCat = new MediaCatalog();
+
+            Media m1 = new Media(mCat.NextMediaID) 
+            { 
+                Author = "H.C. Andersen", 
+                Publisher = "Gyldendal", 
+                Title = "Ugly Duckling", 
+                Year = 2018 
+            };
+            mCat.AddMedia(m1);
+
+            Media m2 = new Media(mCat.NextMediaID)
             {
                 Author = "George Orwell",
                 Publisher = "Pearson",
                 Title = "1984",
                 Year = 2016
             };
+            mCat.AddMedia(m2);
 
-            Media m3 = new Media(3)
+            Media m3 = new Media(mCat.NextMediaID)
             {
                 Author = "James Joyce",
                 Publisher = "Pearson",
-                Title = "Olysses",
+                Title = "Ulysses",
                 Year = 2014
             };
+            mCat.AddMedia(m3);
 
             Console.WriteLine(m1);
             Console.WriteLine(m2);
