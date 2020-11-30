461dae6d06f36f035d16d93ac84dd1cefc28fe93
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2020-11-30 11:37:01 +0100

    Intermediate

---
+++ Media.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaCatalog
+{
+    class Media
+    {
+        public Media(int ID )
+        {
+            MediaID = ID;
+        }
+        public int MediaID { get; private set; }
+
+        public string Author { get; set; }
+        public string Title { get; set; }
+        
+        public int  Year { get; set; }
+
+        public string Publisher { get; set; }
+
+        override public string ToString()
+        {
+            return $"MediaID: {MediaID}, Author: {Author}, Title: {Title}, Year: {Year}, Publisher: {Publisher}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace MediaCatalog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Media catalog starting");
+            Media m1 = new Media(1) { Author = "H.C. Andersen", 
+                                      Publisher = "Gyldendal", 
+                                      Title = "Ugly Duckling", 
+                                      Year = 2018 };
+            Media m2 = new Media(2)
+            {
+                Author = "George Orwell",
+                Publisher = "Pearson",
+                Title = "1984",
+                Year = 2016
+            };
+
+            Media m3 = new Media(3)
+            {
+                Author = "James Joyce",
+                Publisher = "Pearson",
+                Title = "Olysses",
+                Year = 2014
+            };
+
+            Console.WriteLine(m1);
+            Console.WriteLine(m2);
+            Console.WriteLine(m3);
+
+            Console.WriteLine("Media catalog ending");
+    }
     }
 }
